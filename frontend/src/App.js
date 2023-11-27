fc988683901e701d4bcefac135c40245a2816750
Gary <g@modernhuman.xyz>
2023-11-27 16:54:55 -0500

    Add sticky navbar, transparent css transition, general splash styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,11 +4,9 @@ import { Switch } from 'react-router-dom';
 
 import { AuthRoute, ProtectedRoute } from './components/Routes/Routes.js';
 import NavBar from './components/NavBar/NavBar';
+import Footer from './components/Footer/Footer';
 
 import SplashPage from './components/SplashPage/SplashPage.js';
-import Tweets from './components/Tweets/TweetBox.js';
-import Profile from './components/Profile/Profile';
-import TweetCompose from './components/Tweets/TweetCompose';
 
 import { getCurrentUser } from './store/session';
 
@@ -30,6 +28,7 @@ function App() {
         {/* <ProtectedRoute exact path="/profile" component={Profile} /> */}
         {/* <ProtectedRoute exact path="/tweets/new" component={TweetCompose} /> */}
       </Switch>
+      <Footer />
     </>
   );
 }
--- frontend/src/components/SplashPage/SplashPage.js
+++ frontend/src/components/SplashPage/SplashPage.js
@@ -1,13 +1,72 @@
 import './SplashPage.css'
 
-function MainPage() {
+function SplashPage() {
   return (
     <>
-      <footer>
-        Copyright &copy; 2023 Illuminati Inc. dba 'Pacer'
-      </footer>
+      <main>
+        <div className='section-container'>
+          <div className='video-background'>
+            <video autoPlay loop muted playsInline>
+              <source src='../../static/splash-vid-1.mp4' type="video/mp4"/>
+            </video>
+            <div className='video-overlay'>
+              <h1>Hello from MainPage</h1>
+              <h3>Subhead</h3>
+              <p>Lorem ipsum words and words and wordsords and words and wordsords and words and words</p>
+            </div>
+          </div>
+        </div>
+
+        <div className='section-container'>
+          <h1>Hello from MainPage</h1>
+          <h3>Subhead</h3>
+          <p>Lorem ipsum words and words and wordsords and words and wordsords and words and words</p>
+        </div>
+
+        <div className='section-container'>
+          <h1>Hello from MainPage</h1>
+          <h3>Subhead</h3>
+          <p>Lorem ipsum words and words and wordsords and words and wordsords and words and words</p>
+        </div>
+
+        <div className='section-container'>
+          <h1>Hello from MainPage</h1>
+          <h3>Subhead</h3>
+          <p>Lorem ipsum words and words and wordsords and words and wordsords and words and words</p>
+        </div>
+
+        <div className='section-container'>
+          <h1>Hello from MainPage</h1>
+          <h3>Subhead</h3>
+          <p>Lorem ipsum words and words and wordsords and words and wordsords and words and words</p>
+        </div>
+
+        <div className='section-container'>
+          <h1>Hello from MainPage</h1>
+          <h3>Subhead</h3>
+          <p>Lorem ipsum words and words and wordsords and words and wordsords and words and words</p>
+        </div>
+
+        <div className='section-container'>
+          <h1>Hello from MainPage</h1>
+          <h3>Subhead</h3>
+          <p>Lorem ipsum words and words and wordsords and words and wordsords and words and words</p>
+        </div>
+
+        <div className='section-container'>
+          <h1>Hello from MainPage</h1>
+          <h3>Subhead</h3>
+          <p>Lorem ipsum words and words and wordsords and words and wordsords and words and words</p>
+        </div>
+        <div id="gif">
+          
+        </div>
+        <div id="about"></div>
+
+
+      </main>
     </>
   );
 }
 
-export default MainPage;
+export default SplashPage;
