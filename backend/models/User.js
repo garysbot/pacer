620de932e19a7b6980cd32e2502a84de14e1ca47
Francis Cawog <Franciscawog@gmail.com>
2023-11-27 10:57:55 -0500

    changed user model

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,16 +2,40 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true
-  },
   email: {
     type: String,
     required: true
   },
   hashedPassword: {
     type: String,
+    required: true
+  },
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  dateOfBirth: {
+    type: String,
+    required: true
+  },
+  gender: {
+    type: String,
+    required: true
+  },
+  height: {
+    type: String,
+    required: false
+  },
+  weight: {
+    type: String,
+    required: false
+  },
+  friends: {
+    type: Array,
     required: true
   }
 }, {
