df91ec263c6b1b70b5f7b5fe9a6247f0d92a9aaf
Gary <g@modernhuman.xyz>
2023-11-27 17:11:25 -0500

    Finish SplashPage ATF video bg

--- frontend/src/components/SplashPage/SplashPage.js
+++ frontend/src/components/SplashPage/SplashPage.js
@@ -4,16 +4,13 @@ function SplashPage() {
   return (
     <>
       <main>
-        <div className='section-container'>
-          <div className='video-background'>
-            <video autoPlay loop muted playsInline>
-              <source src='../../static/splash-vid-1.mp4' type="video/mp4"/>
-            </video>
-            <div className='video-overlay'>
-              <h1>Hello from MainPage</h1>
-              <h3>Subhead</h3>
-              <p>Lorem ipsum words and words and wordsords and words and wordsords and words and words</p>
-            </div>
+        <div className='video-background'>
+          <video autoPlay loop muted playsInline>
+            <source src='../../static/splash-vid-1.mp4' type="video/mp4"/>
+          </video>
+          <div className='video-overlay'>
+            <h1>Exercising is lonely af</h1>
+            <p>Pacer connects you with new exercise pals at your performance level</p>
           </div>
         </div>
 
