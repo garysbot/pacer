7bbe3a5874950e8b15d90e13d0d73c74d7172f03
DispicableLee <lee.robert053@gmail.com>
2023-11-27 11:33:17 -0500

    not done with event post route but that comes later

--- backend/models/Event.js
+++ backend/models/Event.js
@@ -1,17 +1,37 @@
-// const mongoose = require('mongoose');
-// const Schema = mongoose.Schema;
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-// const tweetSchema = new Schema({
-//   author: {
-//     type: Schema.Types.ObjectId,
-//     ref: 'User'
-//   },
-//   text: {
-//     type: String,
-//     required: true
-//   }
-// }, {
-//   timestamps: true
-// });
+const eventSchema = new Schema({
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  eventName:{ 
+    type:String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  locationName: {
+    type: String,
+    required: true
+  },
+  dateTime: {
+    type: String,
+    required: true
+  },
+  type: String,
+  difficulty: String,
+  maxGroupSize: Integer,
+  attending: Array,
+  longitude: Integer,
+  latitude: Integer
 
-// module.exports = mongoose.model('Tweet', tweetSchema);
+
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('Event', eventSchema);
---
+++ backend/routes/api/events.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
+const Event = mongoose.model('Event')
+const { requireUser } = require('../../config/passport');
+const validateEventInput = require('../../validations/events')
+
+router.get('/', async (req, res) => {
+  try {
+    const events = await Event.find()
+                              .populate("owner", "_id username")
+                              .sort({ createdAt: -1 });
+    return res.json(events);
+  }
+  catch(err) {
+    return res.json([]);
+  }
+});
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const event = await Event.findById(req.params.id)
+                             .populate("owner", "_id username");
+    return res.json(event);
+  }
+  catch(err) {
+    const error = new Error('Tweet not found');
+    error.statusCode = 404;
+    error.errors = { message: "No tweet found with that id" };
+    return next(error);
+  }
+});
