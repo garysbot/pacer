b0f944efe5dc971ff1ae67bc275a3b63cd892330
Gary <g@modernhuman.xyz>
2023-11-29 15:16:07 -0500

    Fixing frontend logic bugs on Events Index Filter

--- frontend/src/components/DiscoverPage/DiscoverPage.js
+++ frontend/src/components/DiscoverPage/DiscoverPage.js
@@ -1,9 +1,7 @@
 import React, { useEffect, useState, useRef } from "react";
-import React, { useEffect, useState, useRef } from "react";
 import './DiscoverPage.css'
 import { useDispatch, useSelector } from "react-redux";
 import { fetchEvents } from "../../store/events";
-import { Link, useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import { Link, useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 export default function DiscoverPage(props){
@@ -74,33 +72,6 @@ export default function DiscoverPage(props){
     const onMouseUpOrLeave = () => {
         setIsDragging(false);
     };
-    // ! For sport-filter-container horizontal mouse scroll
-    // Ref for the container and state for drag-scrolling
-    const sportFilterContainerRef = useRef(null);
-    const [isDragging, setIsDragging] = useState(false);
-    const [startX, setStartX] = useState(0);
-    const [scrollLeft, setScrollLeft] = useState(0);
-
-    // Mouse down handler
-    const onMouseDown = (e) => {
-        setIsDragging(true);
-        setStartX(e.pageX - sportFilterContainerRef.current.offsetLeft);
-        setScrollLeft(sportFilterContainerRef.current.scrollLeft);
-    };
-
-    // Mouse move handler
-    const onMouseMove = (e) => {
-        if (!isDragging) return;
-        e.preventDefault();
-        const x = e.pageX - sportFilterContainerRef.current.offsetLeft;
-        const walk = (x - startX) * 3; // Scroll-fastness
-        sportFilterContainerRef.current.scrollLeft = scrollLeft - walk;
-    };
-
-    // Mouse up and leave handlers
-    const onMouseUpOrLeave = () => {
-        setIsDragging(false);
-    };
 
     return (
         <>
@@ -116,9 +87,6 @@ export default function DiscoverPage(props){
                         <div className="index-header">
                             <h2>Find an event near you</h2>
                         </div>
-
-<<<<<<< HEAD
-<<<<<<< HEAD
                         <div className="sport-filter-container">
                             <p onClick={()=>setRenderedEvents(events)}>Remove Filters</p>
                             {sportsWithEmojis.map((sport)=>{
@@ -130,9 +98,6 @@ export default function DiscoverPage(props){
                                     </p>
                                 )
                             })}
-=======
-=======
->>>>>>> b59b9a814bc8550b627d767ec1a2ad67eabc9703
                         <div 
                             className="sport-filter-container"
                             ref={sportFilterContainerRef}
@@ -151,10 +116,6 @@ export default function DiscoverPage(props){
                                 <p className="sport-label">Pickleball 🥒</p>
                                 <p className="sport-label">Yoga 🧘‍♀️</p>
                                 <p className="sport-label">Walking 🚶</p>
-<<<<<<< HEAD
->>>>>>> b59b9a814bc8550b627d767ec1a2ad67eabc9703
-=======
->>>>>>> b59b9a814bc8550b627d767ec1a2ad67eabc9703
                         </div>
                         <button id="event-create-button"
                             onClick={()=>history.push("/event-form")}
@@ -193,39 +154,11 @@ export default function DiscoverPage(props){
                                             </div>
                                         </div>
                                     </Link>
-                                    <Link to={`/events/${event._id}`}>
-                                        <div key={index} className="event-container">
-                                            <div className="event-content">
-                                                <div className="event-header">
-                                                    <h3>{event.eventName}</h3>
-                                                    <div className="event-subheader">
-                                                        <p className="sport-icon">🏃🏻‍♂️</p>
-                                                        <p className="event-subheader-difficulty">{event.difficulty}</p>
-                                                        <p className="event-subheader-host">with {`${event.ownerDetails.firstName} ${event.ownerDetails.lastName}`}</p>
-                                                    </div>
-                                                </div>
-                                                <div className="date-time-location">
-                                                    { timeConverter(event.dateTime) }
-                                                    <p>{event.locationName}</p>
-                                                    <p>{`${event.attendees.length} / ${event.maxGroupSize} Attendees`}</p>
-                                                </div>
-                                                <div className="event-description">
-                                                    {event.description}
-                                                </div>
-                                            </div>
-                                            <div className="event-mini-map-container">
-                                                <img
-                                                    src={`https://maps.googleapis.com/maps/api/staticmap?center=${event?.latitude},${event?.longitude}&zoom=12&size=400x400&markers=color:red%7Clabel:A%7C${event?.latitude},${event?.longitude}&key=${process.env.REACT_APP_MAPS_API_KEY}`}
-                                                    alt="map"
-                                                />
-                                                {/* <img src='/static/temp-map.png' alt='map' /> */}
-                                            </div>
-                                        </div>
-                                    </Link>
                                 )
                             )
                         }
                     </div>
+                </div>
                 </div>
             </main>
         </>
