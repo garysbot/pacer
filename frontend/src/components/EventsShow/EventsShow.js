91f52f49efb2ae73a155bd436cfa8738902938dc
Francis Cawog <Franciscawog@gmail.com>
2023-11-29 11:26:24 -0500

    added new html to eventsshow

--- frontend/src/components/EventsShow/EventsShow.js
+++ frontend/src/components/EventsShow/EventsShow.js
@@ -9,20 +9,39 @@ export default function EventsShow(){
 
     const dispatch = useDispatch();
     const { id } = useParams();
+    let eventKey;
 
     useEffect(() => {
         dispatch(getEventThunk(id));
     }, [dispatch, id]);
-    debugger;
 
-    const selectedEvent = useSelector((state) => state.events.selectedEvent);
+    const selectedEvent = useSelector((state) => state.events.all);
+
+    if (selectedEvent && Object.keys(selectedEvent).length > 0) {
+        eventKey = Object.keys(selectedEvent)[0];
+    }
+
+    const timeConverter = (dateTime) => {
+        const date = new Date(dateTime);
+        const formattedDate = date.toLocaleDateString("en-US", { year: '2-digit', month: '2-digit', day: '2-digit' });
+        const formattedTime = date.toLocaleTimeString("en-US", { hour: 'numeric', minute: '2-digit', hour12: true });
+
+        return (
+            <p className="date-time"> {selectedEvent[eventKey]?.locationName} - {formattedDate} {formattedTime}</p>
+        )
+    }
 
     return (
         <>
             <p id="event-edit">Edit Event</p>
             <div className="name-box">  
-                {/* <p>{selectedEvent.difficulty}</p> */}
-                
+                <p>{selectedEvent[eventKey]?.eventName}</p>
+            </div>
+            <div className="event-info">
+                <p>{timeConverter(selectedEvent[eventKey]?.dateTime)}</p>
+                <p>{selectedEvent[eventKey]?.eventType} - {selectedEvent[eventKey]?.difficulty}</p>
+                <p>Event created by {selectedEvent[eventKey]?.ownerDetails.firstName} {selectedEvent[eventKey]?.ownerDetails.lastName}</p>
+                <p>{selectedEvent[eventKey]?.description}</p>
             </div>
 
         </>
