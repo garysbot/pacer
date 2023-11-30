9aef3d53662a3d688b5a3abd913b921f6a1f8ea5
DispicableLee <lee.robert053@gmail.com>
2023-11-30 16:06:26 -0500

    made the about page

---
+++ frontend/src/components/About/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './About.css'
+
+export default function About(){
+    return (
+        <div>
+            <h1>About Us</h1>
+            <h3>
+Welcome to Pacer, your go-to social hub for fitness enthusiasts! At Pacer, we believe that exercising is not just about breaking a sweat but also about building connections and having fun. Whether you're a seasoned fitness guru or just starting your wellness journey, Pacer is the perfect platform to find like-minded individuals eager to share their passion for health and fitness.</h3>
+            - Jason, Rob, Francis, Gary, garysbot
+        </div>
+    )
+}
