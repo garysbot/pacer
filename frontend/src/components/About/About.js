108c3273cc92fa70b5cc42308e8783ab8da94870
Gary <g@modernhuman.xyz>
2023-12-03 09:10:30 -0500

    Add most of component structure for about team page

--- frontend/src/components/About/About.js
+++ frontend/src/components/About/About.js
@@ -1,13 +1,251 @@
 import React from "react";
 import './About.css'
 
+
+
+
 export default function About(){
     return (
-        <div>
-            <h1>About Us</h1>
-            <h3>
-Welcome to Pacer, your go-to social hub for fitness enthusiasts! At Pacer, we believe that exercising is not just about breaking a sweat but also about building connections and having fun. Whether you're a seasoned fitness guru or just starting your wellness journey, Pacer is the perfect platform to find like-minded individuals eager to share their passion for health and fitness.</h3>
-            - Jason, Rob, Francis, Gary, garysbot
+        <div className="about-team-parent-container">
+            <div className="about-team-content">
+                <div className="about-team-header">
+
+                    <h1>About Us</h1>
+                    <h2>Subheader lorem ipsum random text here</h2>
+                    <p>
+                        Welcome to Pacer, your go-to social hub for fitness enthusiasts! At Pacer, we believe that exercising is not just about breaking a sweat but also about building connections and having fun. Whether you're a seasoned fitness guru or just starting your wellness journey, Pacer is the perfect platform to find like-minded individuals eager to share their passion for health and fitness.
+                    </p>
+                    <span>- Jason, Rob, Francis, Gary, garysbot</span>
+                </div>
+
+                <div className="about-team-section">
+                    {/* // ! Gary */ }
+                    <div className="person-container">
+                        <h2>Gary Jiang</h2>
+                        <h3>A tagline that's concise</h3>
+                        <p>
+                            And a paragraph to knock it out of the park with a bunch of detail about ourselves. Enough to make us sound cool but not too much where we're gloating and pompous.
+                        </p>
+                        <img src='/static/about-prof-test.png' alt='' className="about-img"/>
+                        <div className="team-link-icons">
+                            <p>
+                                <a
+                                    href='https://google.com'
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="external-link"
+                                >
+                                    <img src='' alt=''/>
+                                    ✅
+                                </a>
+                            </p>
+                            <p>
+                                <a
+                                    href='https://google.com'
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="external-link"
+                                >
+                                    <img src='' alt=''/>
+                                    ✅
+                                </a>
+                            </p>
+                            <p>
+                                <a
+                                    href='https://google.com'
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="external-link"
+                                >
+                                    <img src='' alt=''/>
+                                    ✅
+                                </a>
+                            </p>
+                            <p>
+                                <a
+                                    href='https://google.com'
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="external-link"
+                                >
+                                    <img src='' alt=''/>
+                                    ✅
+                                </a>
+                            </p>
+                        </div>
+                    </div>
+
+                    {/* // ! Jason */ }
+                    <div className="person-container">
+                        <h2>Jason Jun</h2>
+                        <h3>A tagline that's concise</h3>
+                        <p>
+                            And a paragraph to knock it out of the park with a bunch of detail about ourselves. Enough to make us sound cool but not too much where we're gloating and pompous.
+                        </p>
+                        <img src='/static/about-prof-test.png' alt='' className="about-img"/>
+                        <div className="team-link-icons">
+                            <p>
+                                <a
+                                    href='https://google.com'
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="external-link"
+                                >
+                                    <img src='' alt=''/>
+                                    ✅
+                                </a>
+                            </p>
+                            <p>
+                                <a
+                                    href='https://google.com'
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="external-link"
+                                >
+                                    <img src='' alt=''/>
+                                    ✅
+                                </a>
+                            </p>
+                            <p>
+                                <a
+                                    href='https://google.com'
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="external-link"
+                                >
+                                    <img src='' alt=''/>
+                                    ✅
+                                </a>
+                            </p>
+                            <p>
+                                <a
+                                    href='https://google.com'
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="external-link"
+                                >
+                                    <img src='' alt=''/>
+                                    ✅
+                                </a>
+                            </p>
+                        </div>
+                    </div>
+
+                    {/* // ! Francis */ }
+                    <div className="person-container">
+                        <h2>Francis Cawog</h2>
+                        <h3>A tagline that's concise</h3>
+                        <p>
+                            And a paragraph to knock it out of the park with a bunch of detail about ourselves. Enough to make us sound cool but not too much where we're gloating and pompous.
+                        </p>
+                        <img src='/static/about-prof-test.png' alt='' className="about-img"/>
+                        <div className="team-link-icons">
+                            <p>
+                                <a
+                                    href='https://google.com'
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="external-link"
+                                >
+                                    <img src='' alt=''/>
+                                    ✅
+                                </a>
+                            </p>
+                            <p>
+                                <a
+                                    href='https://google.com'
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="external-link"
+                                >
+                                    <img src='' alt=''/>
+                                    ✅
+                                </a>
+                            </p>
+                            <p>
+                                <a
+                                    href='https://google.com'
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="external-link"
+                                >
+                                    <img src='' alt=''/>
+                                    ✅
+                                </a>
+                            </p>
+                            <p>
+                                <a
+                                    href='https://google.com'
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="external-link"
+                                >
+                                    <img src='' alt=''/>
+                                    ✅
+                                </a>
+                            </p>
+                        </div>
+                    </div>
+
+                    {/* // ! Rob */ }
+                    <div className="person-container">
+                        <h2>Rob Lee</h2>
+                        <h3>A tagline that's concise</h3>
+                        <p>
+                            And a paragraph to knock it out of the park with a bunch of detail about ourselves. Enough to make us sound cool but not too much where we're gloating and pompous.
+                        </p>
+                        <img src='/static/about-prof-test.png' alt='' className="about-img"/>
+                        <div className="team-link-icons">
+                            <p>
+                                <a
+                                    href='https://google.com'
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="external-link"
+                                >
+                                    <img src='' alt=''/>
+                                    ✅
+                                </a>
+                            </p>
+                            <p>
+                                <a
+                                    href='https://google.com'
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="external-link"
+                                >
+                                    <img src='' alt=''/>
+                                    ✅
+                                </a>
+                            </p>
+                            <p>
+                                <a
+                                    href='https://google.com'
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="external-link"
+                                >
+                                    <img src='' alt=''/>
+                                    ✅
+                                </a>
+                            </p>
+                            <p>
+                                <a
+                                    href='https://google.com'
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="external-link"
+                                >
+                                    <img src='' alt=''/>
+                                    ✅
+                                </a>
+                            </p>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
         </div>
     )
 }
